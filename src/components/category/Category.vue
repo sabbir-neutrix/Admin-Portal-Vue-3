290c96de4cda478f3872943b6a4d80efeb54e012
sabbir-neutrix <sabbir@neutrix.co>
2023-09-25 17:09:40 +0600

    commit

---
+++ src/components/category/Category.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>I am category</h1>
+</template>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
+import Category from "../components/category/Category.vue";
 
 const routes = [
     { path: "/", redirect: { name: 'Home' } },
-    { path: "/home", name: "Home", component: Home}
+    { path: "/home", name: "Home", component: Home},
+    { path: "/category", name: "Category", component: Category}
 ];
 
 const router = createRouter({
