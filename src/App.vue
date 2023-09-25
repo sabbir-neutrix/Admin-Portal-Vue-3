108cd374aa1b0b4fa88d33547e9617e512d06556
sazidhayatkhan <sazidhayatkhan@gmail.com>
2023-09-25 17:09:58 +0600

    first

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
         <Header></Header>
+        <div class="grid grid-cols-8 gap-4">
+            <div class="bg-red-600 h-screen col-span-2">
+                <Siderbar></Siderbar>
+            </div>
+            <div class="bg-green-600 h-screen col-span-6">
+                <MainContent></MainContent>
+            </div>
+
+        </div>
         <Footer></Footer>
-        <Siderbar></Siderbar>
         <router-view></router-view>
     </div>
 </template>
@@ -11,5 +19,6 @@
 import Header from "./components/layout/Header.vue";
 import Footer from "./components/layout/Footer.vue";
 import Siderbar from "./components/layout/Sidebar.vue";
+import MainContent from "./components/layout/MainContent.vue"
 
 </script>
