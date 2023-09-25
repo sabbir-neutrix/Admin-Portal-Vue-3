7493c97ce54dccbd03bc2fb47db9378f307e9b79
sabbir-neutrix <sabbir@neutrix.co>
2023-09-25 16:17:09 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Header></Header>
+        <Footer></Footer>
+        <Siderbar></Siderbar>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script setup>
+import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
+import Siderbar from "./components/layout/Sidebar.vue";
+
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Hello Vue Router</h1>
+</template>
+
+<script setup>
+
+</script>
