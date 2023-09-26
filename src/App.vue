f541de592d628552ac0f6af738fdbae64f9f2138
sazidhayatkhan <sazidhayatkhan@gmail.com>
2023-09-26 18:11:08 +0600

    dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid grid-cols-10">
-        <div class="bg-red-200 col-span-2 hidden md:block">
+        <div class="col-span-2 hidden md:block border-r-2">
             <Siderbar></Siderbar>
         </div>
         <div class="col-span-10 md:col-span-8">
--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,18 +1,43 @@
 <template>
     <div class="p-4">
+        <div class="mb-7">
+          <h1 class="text-3xl font-semibold">Super Admin</h1>
+        </div>
         <div>
             <button v-for="(buttons, index) in buttons" :key="index" class="bg-slate-400 w-full p-2 mb-4">
                 {{buttons.title}}
             </button>
         </div>
+        <button @click="toggleStatus" class="flex justify-between items-center bg-red-500 w-full p-2">
+          <p class="">SeeMore</p>
+          <p v-if="buttonStatus == true">hide</p>
+          <p v-else>></p>
+        </button>
+        <div v-if="buttonStatus == true" class="px-4 py-2">
+          <div>
+            <button>Button1</button>
+          </div>
+          <div>
+            <button>Button2</button>
+          </div>
+          <div>
+            <button>Button2</button>
+          </div>
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      buttons: [
+<script setup>
+import { ref } from '@vue/reactivity';
+
+const buttonStatus = ref(false);
+
+const toggleStatus = () => {
+    buttonStatus.value = !buttonStatus.value;
+}
+
+
+      const buttons = [
         {
           title: "Category",
           icon: "",
@@ -33,8 +58,5 @@ export default {
           icon: "",
           url: "Laborum deleniti voluptatum distinctio quibusdam iusto, rem quasi in asperiores",
         },
-      ],
-    };
-  },
-};
+      ]
 </script>
