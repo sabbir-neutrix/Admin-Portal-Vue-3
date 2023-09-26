11cb714a34345fd9b2774f58f0c2fd190ca644ff
sabbir-neutrix <sabbir@neutrix.co>
2023-09-26 11:59:07 +0600

    commit

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -3,9 +3,21 @@
         <div class="">
             <div class="flex justify-end items-center _container gap-3">
                 <img src="../../../public/images/test.jpg" alt="" class="w-[40px] h-[40px] object-cover rounded-full" />
-                <button class="font-semibold">Admin</button>
-
+                <button @click="toggleStatus" class="font-semibold">Admin</button>
+                <div v-if="buttonStatus == true">
+                    <p class="text-3xl bg-red-500">Test component</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from '@vue/reactivity';
+
+const buttonStatus = ref(false);
+
+const toggleStatus = () => {
+    buttonStatus.value = !buttonStatus.value;
+}
+</script>
