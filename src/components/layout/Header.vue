72d571ffaf2ca3915d59743c2bcc76df8e1c8bd1
sazidhayatkhan <sazidhayatkhan@gmail.com>
2023-09-26 12:58:41 +0600

    working

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -3,9 +3,15 @@
         <div class="">
             <div class="flex justify-end items-center _container gap-3">
                 <img src="../../../public/images/test.jpg" alt="" class="w-[40px] h-[40px] object-cover rounded-full" />
-                <button @click="toggleStatus" class="font-semibold">Admin</button>
-                <div v-if="buttonStatus == true">
-                    <p class="text-3xl bg-red-500">Test component</p>
+                <div class="relative" v-click-outside="closeToggle">
+                    <button @click="toggleStatus" class="font-semibold">Admin</button>
+                    <div v-if="buttonStatus == true" class="absolute top-[40px] left-[-150px] bg-white border rounded-md drop-shadow overflow-hidden z-20 w-[200px]">
+                        <div>
+                            <button class="text-start p-2 text-black hover:text-white bg-transparent hover:bg-slate-400 transition duration-500 w-full">Profile</button>
+                            <button class="text-start p-2 text-black hover:text-white bg-transparent hover:bg-slate-400 transition duration-500 w-full">Change Password</button>
+                            <button class="text-start p-2 text-black hover:text-white bg-transparent hover:bg-slate-400 transition duration-500 w-full">Log Out</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,4 +26,10 @@ const buttonStatus = ref(false);
 const toggleStatus = () => {
     buttonStatus.value = !buttonStatus.value;
 }
+const closeToggle = () => {
+  if (buttonStatus.value) {
+    buttonStatus.value = false;
+  }
+};
+
 </script>
