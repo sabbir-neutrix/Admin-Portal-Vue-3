a544e9f5d4d610f61161b390d9d6e3529dc6ce74
sazidhayatkhan <sazidhayatkhan@gmail.com>
2023-09-26 11:41:41 +0600

    category

--- src/components/category/Category.vue
+++ src/components/category/Category.vue
@@ -18,9 +18,9 @@
                     <img :src="category.image" alt="" class="w-[70px] h-[70px] object-cover rounded-full" />
                 </div>
                 <div class="w-[70%]">
-                    <p class="text-xl font-semibold line-clamp-2">{{ category.title }}</p>
-                    <p class="text-base font-medium leading-5 mb-2 line-clamp-2">{{ category.description }}</p>
-                    <p class="text-sm leading-5 line-clamp-2">{{ category.additionalInfo }}</p>
+                    <p class="text-base md:text-xl font-semibold line-clamp-2">{{ category.title }}</p>
+                    <p class="text-sm md:text-base font-medium leading-5 mb-1 md:mb-2 line-clamp-2">{{ category.description }}</p>
+                    <p class="text-xs md:text-sm leading-5 line-clamp-2">{{ category.additionalInfo }}</p>
                     <div class="flex justify-start items-center gap-2 mt-3">
                         <button class="bg-slate-400 text-white font-medium rounded-full px-5 py-1">Edit</button>
                         <button class="bg-red-600 text-white font-medium rounded-full px-5 py-1">Delete</button>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -4,6 +4,7 @@
             <div class="flex justify-end items-center _container gap-3">
                 <img src="../../../public/images/test.jpg" alt="" class="w-[40px] h-[40px] object-cover rounded-full" />
                 <button class="font-semibold">Admin</button>
+
             </div>
         </div>
     </div>
