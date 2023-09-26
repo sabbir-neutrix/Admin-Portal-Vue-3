f4bfdb1322e29a24840ee2c6ea71869fc147306a
sabbir-neutrix <sabbir@neutrix.co>
2023-09-26 15:45:01 +0600

    commit

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="bg-slate-200 py-3">
+    <div class="bg-slate-200 py-3" v-click-outside="onClickOutside">
         <div class="">
             <div class="flex justify-end items-center _container gap-3">
                 <img src="../../../public/images/test.jpg" alt="" class="w-[40px] h-[40px] object-cover rounded-full" />
                 <div class="relative">
-                    <button @click="toggleStatus" class="font-semibold">Admin</button>
+                    <button @click="toggleStatus" v-click-out-side="customMethod" class="font-semibold">Admin</button>
                     <div v-if="buttonStatus == true" class="absolute top-[40px] left-[-150px] bg-white border rounded-md drop-shadow overflow-hidden z-20 w-[200px]">
                         <div>
                             <button class="text-start p-2 text-black hover:text-white bg-transparent hover:bg-slate-400 transition duration-500 w-full">Profile</button>
@@ -20,15 +20,19 @@
 
 <script setup>
 import { ref } from '@vue/reactivity';
+import clickOutSide from "@mahdikhashan/vue3-click-outside";
 
+const directives = {
+    clickOutSide
+}
 
+const customMethod = () => {
+    console.log("Outside click.");
+}
 
 const buttonStatus = ref(false);
 
 const toggleStatus = () => {
     buttonStatus.value = !buttonStatus.value;
 }
-
-
-
 </script>
