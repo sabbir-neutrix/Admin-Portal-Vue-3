7daa052625925182b6d2f94c4973c1dacc2e063a
sabbir-neutrix <sabbir@neutrix.co>
2023-09-27 17:31:56 +0600

    commit

--- src/main.js
+++ src/main.js
@@ -4,8 +4,28 @@ import App from './App.vue';
 import './index.css';
 import router from './routes/index';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import moment from 'moment';
+import server from '../config/server.json';
+import api from '../config/api.json';
+
+const filters = {
+    formatDateTime(date) {
+        return moment(String(date)).format('MM/DD/YYYY hh:mm:ss A');
+    },
+    formatTime(date) {
+        return moment(String(date)).format('hh:mm:ss A');
+    },
+    formatDate(date) {
+        return moment(String(date)).format('MM-DD-YYYY');
+    }
+};
 
 const app = createApp(App);
+app.use(VueAxios, axios);
+app.provide('axios', app.config.globalProperties.axios);
+app.provide('filters', app.config.globalProperties.filters);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 app.mount('#app');
--- src/pages/employee/Employee.vue
+++ src/pages/employee/Employee.vue
@@ -1,53 +1,57 @@
 <template>
     <div class="drop-shadow bg-white p-4 md:p-8">
-      <div class="flex flex-col md:flex-row justify-between items-center">
-        <div class="flex justify-start items-center gap-3 mb-6 md:mb-0">
-            <p class="text-xs md:text-sm font-semibold">Home</p>
-            <p class="text-xs md:text-sm font-semibold">></p>
-            <p class="text-xs md:text-sm font-semibold">Employee List</p>
+        <div class="flex flex-col md:flex-row justify-between items-center">
+            <div class="flex justify-start items-center gap-3 mb-6 md:mb-0">
+                <p class="text-xs md:text-sm font-semibold">Home</p>
+                <p class="text-xs md:text-sm font-semibold">></p>
+                <p class="text-xs md:text-sm font-semibold">Employee List</p>
+            </div>
+            <div class="">
+                <router-link to="/add-employee" class="text-sm md:text-base font-medium px-4 md:px-6 py-2 md:py-4 bg-slate-400 text-white rounded-md">
+                    +Add New Employee
+                </router-link>
+            </div>
         </div>
-        <div class="">
-            <router-link to="/add-employee" class="text-sm md:text-base font-medium px-4 md:px-6 py-2 md:py-4 bg-slate-400 text-white rounded-md">+ Add New Employee</router-link>
-        </div>
-      </div>
-        <hr class="my-6 md:my-10 border-b-2" />
+        <hr class="my-6 md:my-10 border-b-2"/>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
             <CardWithoutImage :employees="employees"></CardWithoutImage>
         </div>
     </div>
 </template>
 <script setup>
+import { ref } from 'vue';
 import CardWithoutImage from "../../components/common-card/CardWithoutImage.vue";
+
 const employees = [
     {
-        sl:1,
-        name:'Test',
-        email:'test@gmail.com',
-        phone:'1234567'
+        sl: 1,
+        name: 'Test',
+        email: 'test@gmail.com',
+        phone: '1234567'
     },
     {
-        sl:2,
-        name:'Test',
-        email:'test@gmail.com',
-        phone:'1234567'
+        sl: 2,
+        name: 'Test',
+        email: 'test@gmail.com',
+        phone: '1234567'
     },
     {
-        sl:3,
-        name:'Test',
-        email:'test@gmail.com',
-        phone:'1234567'
+        sl: 3,
+        name: 'Test',
+        email: 'test@gmail.com',
+        phone: '1234567'
     },
     {
-        sl:4,
-        name:'Test',
-        email:'test@gmail.com',
-        phone:'1234567'
+        sl: 4,
+        name: 'Test',
+        email: 'test@gmail.com',
+        phone: '1234567'
     },
     {
-        sl:5,
-        name:'Test',
-        email:'test@gmail.com',
-        phone:'1234567'
+        sl: 5,
+        name: 'Test',
+        email: 'test@gmail.com',
+        phone: '1234567'
     },
 ]
 </script>
