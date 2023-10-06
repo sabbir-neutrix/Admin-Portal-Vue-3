eec6531bb36140ebdc58d27f35404df04435c5b0
sazidhayatkhan <sazidhayatkhan@gmail.com>
2023-10-06 10:02:36 +0600

    added

--- src/components/common-card/ContactCard.vue
+++ src/components/common-card/ContactCard.vue
@@ -4,9 +4,9 @@
         <div class="flex justify-between items-center p-3">
             <div class="w-[60%]">
                 <p class="text-sm text-slate-600 mb-2">Digital Stragegist</p>
-                <p class="text-base md:text-xl font-semibold line-clamp-2">Nicola Pierson</p>
-                <p class="text-sm md:text-base font-medium leading-5 mb-1 md:mb-2 line-clamp-2">{{ category.description }}</p>
-                <p class="text-xs md:text-sm leading-5 line-clamp-2">{{ category.additionalInfo }}</p>
+                <p class="text-base md:text-xl font-semibold line-clamp-2 mb-2">Nicola Pierson</p>
+                <p class="text-sm md:text-xs font-medium leading-5 mb-1 md:mb-2 line-clamp-2">{{ category.description }}</p>
+                <p class="text-xs md:text-xs leading-5 line-clamp-2">{{ category.additionalInfo }}</p>
             </div>
             <div class="w-[40%]">
                 <img :src="category.image" alt="" class="w-[90px] h-[90px] object-cover rounded-full mx-auto" />
@@ -15,9 +15,9 @@
         <div class="flex justify-end items-center gap-2 mt-3 bg-slate-300 px-3 py-2">
                 <!-- <button class="bg-slate-400 text-white font-medium rounded-full px-5 py-1">Edit</button>
                 <button class="bg-red-600 text-white font-medium rounded-full px-5 py-1">Delete</button> -->
-                <button class="bg-slate-600 text-white px-[6px] py-[3px] rounded text-sm"><font-awesome-icon icon="fa-solid fa-pen-to-square" /></button>
-                <button class="bg-slate-600 text-white px-[6px] py-[3px] rounded text-sm"><font-awesome-icon icon="fa-solid fa-eye" /></button>
-                <button class="bg-slate-600 text-white px-[6px] py-[3px] rounded text-sm"><font-awesome-icon icon="fa-solid fa-trash" /></button>
+                <button class=" text-slate-600 px-[6px] py-[3px] rounded text-sm"><font-awesome-icon icon="fa-solid fa-pen-to-square" /></button>
+                <button class=" text-slate-600 px-[6px] py-[3px] rounded text-sm"><font-awesome-icon icon="fa-solid fa-eye" /></button>
+                <button class=" text-slate-600 px-[6px] py-[3px] rounded text-sm"><font-awesome-icon icon="fa-solid fa-trash" /></button>
             </div>
     </div>
 </template>
